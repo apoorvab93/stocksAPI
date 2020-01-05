25500e05c5902fab5573f0a1ba5aa8f1270f054a
Apoorva <Apoorva@DESKTOP-J6ICKRG>
2020-01-05 15:23:40 -0800

    first commit

---
+++ Contracts/Stock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockWebApp.Contracts
+{
+    public class Stock
+    {
+        public int Price;
+
+    }
+}
---
+++ Controllers/StockController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace StockWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        public readonly ILogger logger;
+        public StockController(ILogger<StockController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> GetStockPrice()
+        {
+            using var client = new HttpClient();
+            this.logger.LogInformation("Going to query api robinhood for top 5 stocks");
+            HttpResponseMessage result = await client.GetAsync("https://api.robinhood.com/instruments/?top=5");
+            this.logger.LogInformation("Got results from robinhood");
+            string str = await result.Content.ReadAsStringAsync();
+            return str;
+
+        }
+
+        [HttpGet("{companyName}")]
+        public async Task<string> GetStockPrice(string companyName)
+        {
+            using var client = new HttpClient();
+            HttpResponseMessage result = await client.GetAsync("https://api.robinhood.com/instruments/?symbol="+companyName);
+            string str = await result.Content.ReadAsStringAsync();
+            return str;
+        }
+    }
+}
